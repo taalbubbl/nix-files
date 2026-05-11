50079706d52a4e5279610965030750bfa746bcc5
Your Name <you@example.com>
2026-05-11 13:33:33 +0200

    loki old logs readable

--- modules/analytics.nix
+++ modules/analytics.nix
@@ -12,6 +12,7 @@ with lib; let
       path       = "/var/log/journal"
       forward_to = [loki.write.remote.receiver]
       max_age    = "720h"
+      start_position = "beginning"
       labels = {
         host = constants.hostname,
         job  = "systemd-journal",
