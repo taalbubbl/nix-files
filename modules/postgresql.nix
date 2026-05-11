7e0e28cca8eaeced165878bd9427ef8919404e49
Your Name <you@example.com>
2026-05-11 18:55:34 +0200

    bug fixes

--- modules/postgresql.nix
+++ modules/postgresql.nix
@@ -191,7 +191,6 @@ in
                 archive-push-queue-max=4GB
                 retention-full=${toString cfg.pgbackrest.retention.full}
                 start-fast=y
-                archive-timeout=300
                 spool-path=/var/lib/pgbackrest/spool
 
                     
