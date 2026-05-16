be8ebda9fe5f30674c9b45e8eaa8d5d166fc5941
Your Name <you@example.com>
2026-05-16 11:36:14 +0200

    change pw of user david

--- modules/authelia.nix
+++ modules/authelia.nix
@@ -76,10 +76,9 @@ in {
           client_name = "Vikunja";
           # Hash of vikunja-client-secret. Generate with:
           # authelia crypto hash generate pbkdf2 --random
-          client_secret = "$pbkdf2-sha512$310000$91IwbCDI7zXRBzHeggT/Zg$L2xE6ILl5gWuZrJJl6BabxabmZtjVwt2Cz.bo4eq7qI/4E2nI8uy3p.ve34MLyD.tkSq3TdiptTWF.WOKP66Pw";
+          client_secret = "$pbkdf2-sha512$310000$83cZJwDGDOadBAQfbHzJ8g$ljMg2q44.FCZohmGYi/kSBuvE8wkL91GlG8enw9o174Pjhp1A3kmeBZ0VxxjBwlkCXzmjlrR4kcF6IoIqgP8Yg";
           public = false;
           authorization_policy = "one_factor";
-          token_endpoint_auth_method = "client_secret_post";
           redirect_uris = [ "https://vikunja.taalbubbl.org/auth/openid/authelia" ];
           scopes = [ "openid" "profile" "email" "groups" ];
           userinfo_signed_response_alg = "none";
