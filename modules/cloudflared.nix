f25bfeadc1c2291553e3557ad3f828459e14032d
Your Name <you@example.com>
2026-05-27 13:23:36 +0200

    cloudflared change

--- modules/cloudflared.nix
+++ modules/cloudflared.nix
@@ -1,15 +1,29 @@
 {
-    services.cloudflared = {
-      enable = true;
-      tunnels = {
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
       "8e979e98-e5fe-4d52-9112-ad4b2f10b955" = {
         credentialsFile = "/home/david/.cloudflared/8e979e98-e5fe-4d52-9112-ad4b2f10b955.json";
         default = "http_status:404";
         ingress = {
-          "taalbubbl.org" = "http://127.0.0.1:80";
-          "*.taalbubbl.org" = "http://127.0.0.1:80";
+          "taalbubbl.org" = {
+            service = "https://127.0.0.1:443";
+            originRequest = {
+              originServerName = "taalbubbl.org";
+              noTLSVerify = true;
+            };
+          };
+          "*.taalbubbl.org" = {
+            service = "https://127.0.0.1:443";
+            originRequest = {
+              # SNI will be the requested hostname automatically when noTLSVerify
+              # is true and no originServerName is set on a wildcard. nginx then
+              # picks the vhost via the Host header, which cloudflared forwards.
+              noTLSVerify = true;
+            };
+          };
         };
       };
-      };
     };
+  };
 }
